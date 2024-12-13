d928fc8032d2ab92f7474c86d3e0fc424e298fb0
balejandro2007 <silviachavez1988alejapatricia@gmail.com>
2024-12-13 15:15:37 -0500

    first commit

---
+++ resources/PacienteResource.php
@@ -0,0 +1,65 @@
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PacienteResource\Pages;
+use App\Models\Paciente;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PacienteResource extends Resource
+{
+    protected static ?string $model = Paciente::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static string $slug = 'pacientes';
+
+    protected static ?string $navigationGroup = 'Odontología';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('nombre')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('apellido')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('tratamiento')
+                ->required()
+                ->maxLength(255),
+
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre'),
+                Tables\Columns\TextColumn::make('apellido'),
+                Tables\Columns\TextColumn::make('tratamiento'),
+              
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPacientes::route('/'),
+            'create' => Pages\CreatePaciente::route('/create'),
+            'edit' => Pages\EditPaciente::route('/{record}/edit'),
+        ];
+    }
+}
